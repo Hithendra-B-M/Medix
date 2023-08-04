62897e73077cbb1222921b191e898acd67ced39b
Karthik R <karthikclgid@gmail.com>
2023-08-04 23:14:19 +0530

    PreProcessing for the Flask App

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return '<p>Helloworld!</p>'
+def homepage(): #Function name and the url_for name should be same
+    return render_template('homepage.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/report')
+def report():
+    return render_template('report.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
