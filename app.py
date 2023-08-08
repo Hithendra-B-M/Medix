408a6ca70fa14682c335dc15a95c154542ac73b3
Karthik R <karthikclgid@gmail.com>
2023-08-08 19:41:38 +0530

    update 4.0

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ import io
 
 app = Flask(__name__)
 
+model = load_model("Voidex.h5")
+
+class_labels = [
+    "Atelectasis", "Brain_Tumor", "Cardiomegaly", "Consolidation", "Edema", "Effusion",
+    "Emphysema", "Fibrosis", "Hernia", "Infiltration", "Mass", "No_Brain_Finding",
+    "No_Lung_Finding", "Nodule", "Pleural", "Pneumonia", "Pneumothorax", "Tuberculosis"
+]
+
 @app.route('/')
 def homepage(): #Function name and the url_for name should be same
     return render_template('homepage.html')
@@ -22,15 +30,35 @@ def report():
 @app.route("/save", methods=["POST"])
 def save_image():
     if request.method == "POST":
-        file = request.files["image"]
+        image_file = request.files["image"]
+        img = image.load_img(io.BytesIO(image_file.read()), target_size=(150, 150))
+        img = image.img_to_array(img)
+        img = np.expand_dims(img, axis=0)
+        img = img / 255.0
 
-        if file:
-            filename = file.filename
-            # file.save(os.path.join("uploads", filename))
+        predictions = model.predict(img)
+        predicted_class_index = np.argmax(predictions[0])
+        predicted_class = class_labels[predicted_class_index]
+        
+        print(img)
 
-            return "1"  # Return 1 if the file was uploaded successfully
-        else:
-            return "0"  # Return 0 if no file was selected
+        return predicted_class
+
+
+@app.route("/classify", methods=["POST"])
+def classify():
+    image_file = request.files["image"]
+    img = image.load_img(io.BytesIO(image_file.read()), target_size=(150, 150))
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = img / 255.0
+
+    predictions = model.predict(img)
+    predicted_class_index = np.argmax(predictions[0])
+    predicted_class = class_labels[predicted_class_index]
+    print(img)
+
+    return predicted_class
 
 if __name__ == '__main__':
     app.run(debug=True)
